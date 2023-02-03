12d882b6c03f7bcf12c8c7fb5dfea1d177315ae8
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-02-03 14:03:45 -0300

    feat: add font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        
         <link rel="icon" type="image/png" href="/favicon.png" />
+        
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
