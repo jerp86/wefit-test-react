f5e427bafb288d1d4c83a91e183966914a995d7c
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-02-03 14:08:10 -0300

    feat: add extends for eslint

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,11 +5,18 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+
         <link rel="icon" type="image/png" href="/favicon.png" />
-        
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet" />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
