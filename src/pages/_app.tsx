d331dd6cd865f27e740a16c5c711ff83e53ac235
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-02-03 15:18:58 -0300

    feat: add style and component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
+import { Header } from '@/components/Header'
 import { globalStyles } from '@/styles/global'
+import { AppContainer } from '@/styles/pages/_app'
 import type { AppProps } from 'next/app'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AppContainer>
+      <Header />
+
+      <Component {...pageProps} />
+    </AppContainer>
+  )
 }
