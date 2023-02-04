856bd7ac1b0635f6242d9223ede1a78e2d1a1240
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-02-04 12:32:16 -0300

    feat: create context

---
+++ src/contexts/CartContext.tsx
@@ -0,0 +1,137 @@
+import produce from 'immer'
+import { ReactNode, useCallback, useEffect, useMemo, useState } from 'react'
+import { createContext, useContextSelector } from 'use-context-selector'
+
+export interface Movie {
+  id: number
+  title: string
+  price: number
+  image: string
+}
+
+export interface CartItem extends Movie {
+  quantity: number
+}
+
+interface ChangeCartItemQuantityProps {
+  itemId: number
+  type: 'increase' | 'decrease'
+}
+
+interface CartContextType {
+  cartItems: CartItem[]
+  cartItemsTotal: number
+  cartQuantity: number
+  addMovieToCart: (movie: CartItem) => void
+  changeCartItemQuantity: (value: ChangeCartItemQuantityProps) => void
+  removeCartItem: (itemId: number) => void
+  cleanCart: () => void
+}
+
+interface CartContextProviderProps {
+  children: ReactNode
+}
+
+const MOVIE_ITEMS_STORAGE_KEY = 'WeMovies:cartItems'
+
+const fetchLocalStorage = (): CartItem[] => {
+  const storedCartItems = localStorage.getItem(MOVIE_ITEMS_STORAGE_KEY)
+  if (storedCartItems) return JSON.parse(storedCartItems)
+
+  return []
+}
+
+export const CartContext = createContext({} as CartContextType)
+
+export const CartContextProvider = ({ children }: CartContextProviderProps) => {
+  const [cartItems, setCartItems] = useState<CartItem[]>(fetchLocalStorage())
+
+  const cartQuantity = cartItems.length
+
+  const cartItemsTotal = useMemo(
+    () =>
+      cartItems.reduce(
+        (totalPrice, movie) => (totalPrice += movie.price * movie.quantity),
+        0,
+      ),
+    [cartItems],
+  )
+
+  const movieExistsInCart = useCallback(
+    (movieId: number) => cartItems.findIndex(({ id }) => id === movieId),
+    [cartItems],
+  )
+
+  const addMovieToCart = useCallback(
+    (movie: CartItem) => {
+      const movieAlreadyExistsInCart = movieExistsInCart(movie.id)
+
+      const newCart = produce(cartItems, (draft) => {
+        if (movieAlreadyExistsInCart < 0) {
+          draft.push(movie)
+        } else {
+          draft[movieAlreadyExistsInCart].quantity += movie.quantity
+        }
+      })
+
+      setCartItems(newCart)
+    },
+    [cartItems, movieExistsInCart],
+  )
+
+  const changeCartItemQuantity = useCallback(
+    ({ itemId, type }: ChangeCartItemQuantityProps) => {
+      const newCart = produce(cartItems, (draft) => {
+        const movieAlreadyExistsInCart = movieExistsInCart(itemId)
+
+        if (movieAlreadyExistsInCart >= 0) {
+          const { quantity } = draft[movieAlreadyExistsInCart]
+          draft[movieAlreadyExistsInCart].quantity =
+            type === 'increase' ? quantity + 1 : quantity - 1
+        }
+      })
+
+      setCartItems(newCart)
+    },
+    [cartItems, movieExistsInCart],
+  )
+
+  const removeCartItem = (itemId: number) => {
+    const newCart = produce(cartItems, (draft) => {
+      const movieAlreadyExistsInCart = movieExistsInCart(itemId)
+
+      if (movieAlreadyExistsInCart >= 0) {
+        draft.splice(movieAlreadyExistsInCart, 1)
+      }
+    })
+
+    setCartItems(newCart)
+  }
+
+  const cleanCart = () => setCartItems([])
+
+  useEffect(() => {
+    localStorage.setItem(MOVIE_ITEMS_STORAGE_KEY, JSON.stringify(cartItems))
+  }, [cartItems])
+
+  return (
+    <CartContext.Provider
+      value={{
+        cartItems,
+        cartItemsTotal,
+        cartQuantity,
+        addMovieToCart,
+        changeCartItemQuantity,
+        cleanCart,
+        removeCartItem,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+export const useCart = () => {
+  const context = useContextSelector(CartContext, (context) => context)
+  return context
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { Header } from '@/components/Header'
+import { CartContextProvider } from '@/contexts/CartContext'
 import { globalStyles } from '@/styles/global'
 import { AppContainer } from '@/styles/pages/_app'
 import type { AppProps } from 'next/app'
@@ -8,9 +9,11 @@ globalStyles()
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppContainer className="container">
-      <Header />
+      <CartContextProvider>
+        <Header />
 
-      <Component {...pageProps} />
+        <Component {...pageProps} />
+      </CartContextProvider>
     </AppContainer>
   )
 }
