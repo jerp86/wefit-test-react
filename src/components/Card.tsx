bd3bbef7cc42c7f7f054cd7a8fa6ff71143aa482
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-02-04 22:19:19 -0300

    feat: add prop, add useCart, update button

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,28 +1,58 @@
+import weFit from '@/assets/wefit.png'
+import { Movie, useCart } from '@/contexts/CartContext'
 import { CardContainer } from '@/styles/components/Card'
+import { formattedPrice } from '@/utils/formattedPrice'
 import Image from 'next/image'
 import { Button } from './Button'
+import { Loader } from './Loader'
 
-export const Card = () => (
-  <CardContainer>
-    <div>
+interface CardProps {
+  product: Movie
+}
+
+export const Card = ({ product }: CardProps) => {
+  const { addMovieToCart, quantityMovieInStorage } = useCart()
+
+  if (!product.id) {
+    return <Loader />
+  }
+
+  const hasCurrentProduct = quantityMovieInStorage(product.id)
+
+  const handleAddProduct = () => {
+    const movieToAdd = { ...product, quantity: 1 }
+    addMovieToCart(movieToAdd)
+  }
+
+  return (
+    <CardContainer>
       <div>
-        <Image
-          src="/x.png"
-          alt=""
-          loading="lazy"
-          decoding="async"
-          width={147}
-          height={188}
-        />
+        <div>
+          <Image
+            src={product.image ?? weFit}
+            alt={`Movie cover image ${product.title}`}
+            loading="lazy"
+            decoding="async"
+            width={147}
+            height={188}
+            placeholder="blur"
+            blurDataURL="https://user-images.githubusercontent.com/54115624/216507723-680f5174-9963-4b6f-b860-605d751aa02f.png"
+          />
 
-        <p>Homem Aranha</p>
+          <p>{product.title}</p>
+        </div>
+
+        <p>{formattedPrice.format(product.price)}</p>
       </div>
 
-      <p>R$ 29,99</p>
-    </div>
-
-    <Button type="button" quantity={0}>
-      adicionar ao carrinho
-    </Button>
-  </CardContainer>
-)
+      <Button
+        type="button"
+        hasIcon
+        onClick={handleAddProduct}
+        disabled={hasCurrentProduct !== 0}
+      >
+        Adicionar ao carrinho
+      </Button>
+    </CardContainer>
+  )
+}
