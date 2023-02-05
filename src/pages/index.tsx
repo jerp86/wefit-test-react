59d937af35f2b9d3366a38393a35a42ebb771306
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-02-04 22:24:16 -0300

    feat: add GetStaticProps and props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,37 @@
 import { ListCards } from '@/components/ListCards'
+import { Movie } from '@/contexts/CartContext'
+import { api } from '@/lib/axios'
+import { GetStaticProps } from 'next'
 
-export default function Home() {
-  return <ListCards />
+interface HomeProps {
+  products: Movie[]
+}
+
+export default function Home({ products }: HomeProps) {
+  return <ListCards products={products} />
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const seconds = 60
+  const minutes = 60
+  const hours = 24
+  const days = 7
+
+  const response = await api.get<Movie[]>('/products')
+
+  const movies = response.data.map((movie) => {
+    return {
+      id: movie.id,
+      title: movie.title,
+      image: movie.image,
+      price: movie.price,
+    } as Movie
+  })
+
+  return {
+    props: {
+      products: movies,
+    },
+    revalidate: seconds * minutes * hours * days,
+  }
 }
